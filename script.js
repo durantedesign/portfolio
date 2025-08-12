1639ee8918fb9d1f41e88ed3d86ec34a46070a9b
Bia Durante Design <biadurante.design@gmail.com>
2025-08-12 17:32:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4, // Aqui define 4 slides em telas maiores
+    },
+  },
+});
